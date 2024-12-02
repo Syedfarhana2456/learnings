31bab241e973b80de43269809f15ac7593068729
syedfarhana2456 <syedfarhana2456@gmail.com>
2024-12-03 01:26:10 +0530

    MemoryAllocationOfDataTypes

---
+++ C_CODES/SizeOfDataType.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ printf("The memory allocation of various data types");
+ printf("size of char %lu\n",sizeof(char));
+ printf("size of int %lu\n",sizeof(int));
+ printf("short int %lu\n",sizeof(short int));
+ printf("unsigned int %lu\n",sizeof(unsigned int));
+ printf("signed int%lu \n",sizeof(signed int));
+ printf("longed int %lu \n",sizeof(long int));
+ printf("unsigned long int \n%lu",sizeof(unsigned long int));
+ printf("float %lu\n",sizeof(float));
+ printf("double %lu\n",sizeof(double));
+ printf("long double \n%lu",sizeof(long double));
+ return 0;
+}
+ 
+ 
