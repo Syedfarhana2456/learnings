fb4f483a6800ed0f21eff5665f6e9b3caee5368a
syedfarhana2456 <syedfarhana2456@gmail.com>
2025-02-24 19:58:58 +0530

    ArrayLists In Java

---
+++ JAVA_PROGRAMS/Lists.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Lists {
+    public static void main(String[] args) {
+        ArrayList<String> names=new ArrayList<>();
+        names.add("alice");
+        names.add("bob");
+        names.add("alice");
+        System.out.println("List:"+names);
+
+    }
+}
